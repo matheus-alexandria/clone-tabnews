be4b4a42a62057b516f3bff358c722fb59a018af
Matchobas <maalexandria@hotmail.com>
2023-12-28 22:36:35 -0300

    fix: minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Página para a Lívia, o ser humano que eu mais amo no mundo</h1>
+  return <h1>Página para o meu bem (Lívia), o ser humano que eu mais amo no mundo</h1>
 }
 
 export default Home;
