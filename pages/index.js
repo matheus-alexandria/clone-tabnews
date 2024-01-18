647525dbffd4896fcee92c53e9fd210e95471ef5
Matchobas <maalexandria@hotmail.com>
2024-01-18 20:12:44 -0300

    refactor: little joke for the site

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import React, { useState } from "react";
+
 function Home() {
+  const [name, setName] = useState('Matheus');
   return (
-    <h1>
-      Rapaz, é domingo e eu to querendo fazer um commit irráaa
-    </h1>
+    <>
+      <h1>
+        {`Eu tenho toda certeza que o ${name} é um otário`}
+      </h1>
+      <select onChange={(event) => { setName(event.target.value) }}>
+        <option value='Diego'>Diego</option>
+        <option value='Marcelo P1'>P1</option>
+        <option value='Marcelo P2'>P2</option>
+        <option value='Douglas'>Douglas</option>
+      </select>
+    </>
   );
 }
 
