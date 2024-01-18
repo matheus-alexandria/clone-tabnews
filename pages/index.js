bc85e3f44e5ed83509a72d3a39b9a6ffe9a590bc
Matchobas <maalexandria@hotmail.com>
2024-01-18 20:57:26 -0300

    chore: add special option

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,25 @@ import React, { useState } from "react";
 
 function Home() {
   const [name, setName] = useState('Matheus');
+
+  function buildPhrase() {
+    if (name === 'Lívia') {
+      return `A ${name} é a pessoa que eu mais amo no mundo`
+    }
+    return `Eu tenho toda certeza que o ${name} é um otário`;
+  }
+
   return (
     <>
       <h1>
-        {`Eu tenho toda certeza que o ${name} é um otário`}
+        {buildPhrase()}
       </h1>
       <select onChange={(event) => { setName(event.target.value) }}>
         <option value='Diego'>Diego</option>
         <option value='Marcelo P1'>P1</option>
         <option value='Marcelo P2'>P2</option>
         <option value='Douglas'>Douglas</option>
+        <option value='Lívia'>Lívia</option>
       </select>
     </>
   );
