4013b4262b6d6811f0cd55d934baadfb1da5e87f
Matchobas <maalexandria@hotmail.com>
2024-02-02 15:52:28 -0300

    test: create new tests for the calculator

--- src/models/calculator.js
+++ src/models/calculator.js
@@ -1,4 +1,5 @@
 function calcSum(num1, num2) {
+  if (typeof num1 !== 'number' | typeof num2 !== 'number' ) return 'Error';
   return num1 + num2;
 }
 
--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,7 +1,7 @@
 const { calcSum } = require('../models/calculator');
 
-test("calculate sum of two numbers", () => {
-  const sum = calcSum(2, 2);
+it.each([[2, 3, 5], [50, 100, 150], ['banana', 5, 'Error']])("should calculate sum of two numbers", (n1, n2, result) => {
+  const sum = calcSum(n1, n2);
 
-  expect(sum).toBe(4);
+  expect(sum).toBe(result);
 });
