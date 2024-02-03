83296e3ab69df7b0e781de8565df1d9a6756fddc
Matchobas <maalexandria@hotmail.com>
2024-02-03 20:37:16 -0300

    refactor: prettier in test file

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,6 +1,10 @@
-const { calcSum } = require('../models/calculator');
+const { calcSum } = require("../models/calculator");
 
-it.each([[2, 3, 5], [50, 100, 150], ['banana', 5, 'Error']])("should calculate sum of two numbers", (n1, n2, result) => {
+it.each([
+  [2, 3, 5],
+  [50, 100, 150],
+  ["banana", 5, "Error"],
+])("should calculate sum of two numbers", (n1, n2, result) => {
   const sum = calcSum(n1, n2);
 
   expect(sum).toBe(result);
