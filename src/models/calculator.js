9a92c1a85d7f27a122592b2831d9223d0bd63f31
Matchobas <maalexandria@hotmail.com>
2024-02-06 23:12:52 -0300

    test: create better error test

--- src/models/calculator.js
+++ src/models/calculator.js
@@ -1,5 +1,5 @@
 function calcSum(num1, num2) {
-  if (typeof num1 !== 'number' | typeof num2 !== 'number' ) return 'Error';
+  if (typeof num1 !== 'number' | typeof num2 !== 'number' ) throw new Error();
   return num1 + num2;
 }
 
--- src/tests/unit/calculator.test.js
+++ src/tests/unit/calculator.test.js
@@ -2,10 +2,13 @@ const { calcSum } = require("../../models/calculator");
 
 it.each([
   [2, 3, 5],
-  [50, 100, 150],
-  ["banana", 5, "Error"],
+  [50, 100, 150]
 ])("should calculate sum of two numbers", (n1, n2, result) => {
   const sum = calcSum(n1, n2);
 
   expect(sum).toBe(result);
+});
+
+it('should throw error when calculator is called with wrong params', () => {
+  expect(() => calcSum(10, 'banana')).toThrow(Error);
 });
