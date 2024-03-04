df7a9405f9475e1e09e8434db7547d152326cd67
Matchobas <maalexandria@hotmail.com>
2024-03-04 20:55:58 -0300

    refactor: change client option for env vars

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import { Client } from 'pg';
 
 async function query(queryString) {
   const client = new Client({
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    user: process.env.DATABASE_USER,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
   });
   await client.connect();
 
