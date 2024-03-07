913af9685e35ef65ccc27afa4dfd9f1b056c6ecf
Matchobas <maalexandria@hotmail.com>
2024-03-06 21:25:16 -0300

    refactor: change path for `/status`

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../../infra/database";
+import database from "infra/database";
 
 export default async function status(request, response) {
   const result = await database.query('SELECT 1 + 1 AS sum;');
