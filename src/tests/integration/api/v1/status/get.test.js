c52dde474d12b347b407eb27c1b5fefae53cf0a2
Matchobas <maalexandria@hotmail.com>
2024-03-11 14:21:23 -0300

    test: adapt test to new response body

--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,10 @@
 it('should GET /api/v1/status and return a 200 status code', async () => {
   const response = await fetch('http://localhost:3000/api/v1/status');
+  const responseBody = await response.json();
 
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
