6857c07d2f6ee2aa0ca6c613f13f4eccc112d6a2
Matchobas <maalexandria@hotmail.com>
2024-03-12 10:26:57 -0300

    feat: add settings data from database

--- src/pages/api/v1/status/index.js
+++ src/pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
 import database from "infra/database";
 
 export default async function status(request, response) {
+  const version = await database.query(
+    `
+      SELECT setting AS version
+      FROM pg_settings
+      WHERE name = 'server_version'
+    `
+  );
+  const maxConnections = await database.query(
+    `
+      SELECT setting AS max_connections
+      FROM pg_settings
+      WHERE name = 'max_connections'
+    `
+  );
+  const connections = await database.query("SELECT pid FROM pg_stat_activity");
+
   const updatedAt = new Date().toISOString();
   response.status(200).json({
-    updated_at: updatedAt
+    updated_at: updatedAt,
+    database: {
+      version: version.rows[0].version,
+      max_connections: Number(maxConnections.rows[0].max_connections),
+      opened_connections: connections.rows.length
+    }
   });
 }
