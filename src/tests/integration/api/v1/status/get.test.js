e8fbd287ce41d020287715aec0b505fa7073664c
Matchobas <maalexandria@hotmail.com>
2024-03-12 10:27:07 -0300

    test: check if database data is correct

--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,13 @@ it('should GET /api/v1/status and return a 200 status code', async () => {
 
   expect(response.status).toBe(200);
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.database).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.database).toEqual(expect.objectContaining({
+    version: expect.any(String),
+    max_connections: expect.any(Number),
+    opened_connections: expect.any(Number)
+  }));
 });
