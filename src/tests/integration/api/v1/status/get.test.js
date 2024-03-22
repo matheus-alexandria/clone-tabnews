b899a7e028dd89499dc325584d7e9fa95fcf0c6d
Matchobas <maalexandria@hotmail.com>
2024-03-22 20:59:06 -0300

    test: make max connection check more robust

--- src/tests/integration/api/v1/status/get.test.js
+++ src/tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,5 @@
 it('should GET /api/v1/status and return a 200 status code', async () => {
-  const response = await fetch(`http://localhost:3001/api/v1/status`);
+  const response = await fetch(`http://localhost:3000/api/v1/status`);
   const responseBody = await response.json();
 
   expect(response.status).toBe(200);
@@ -8,7 +8,8 @@ it('should GET /api/v1/status and return a 200 status code', async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
   expect(responseBody.dependencies.database).toEqual(expect.objectContaining({
     version: expect.any(String),
-    max_connections: expect.any(Number),
     opened_connections: 1
   }));
+
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThanOrEqual(1);
 });
