44baffcddeb472d9305f0e3dbe6a4d7bf8a1844f
Matchobas <maalexandria@hotmail.com>
2024-03-26 23:12:59 -0300

    test: init api dry run migration route test

---
+++ src/tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,7 @@
+it('should GET /api/v1/migrations and return a 200 status code', async () => {
+  const response = await fetch(`http://localhost:3000/api/v1/migrations`);
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(200);
+  expect(Array.isArray(responseBody)).toBeTruthy();
+});
