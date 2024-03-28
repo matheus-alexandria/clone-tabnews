a6e87361decfd9b7b2abd68d17b994eb324c49cd
Matchobas <maalexandria@hotmail.com>
2024-03-27 23:13:46 -0300

    feat: create option for live run when method is POST

--- src/pages/api/v1/migrations/index.js
+++ src/pages/api/v1/migrations/index.js
@@ -2,13 +2,28 @@ import migrationsRunner from 'node-pg-migrate';
 import { join } from 'node:path';
 
 export default async function migrations(request, response) {
-  const migrations = await migrationsRunner({
+  if (request.method === 'GET') {
+    const migrations = await runMigrations(true);
+
+    response.status(200).json(migrations);
+  }
+
+  if (request.method === 'POST') {
+    const migrations = await runMigrations(false);
+
+    response.status(200).json(migrations);
+  }
+
+  response.status(405).json();
+}
+
+async function runMigrations(dryRun) {
+  return await migrationsRunner({
     databaseUrl: process.env.DATABASE_URL,
     dir: join('infra', 'migrations'),
     direction: 'up',
-    dryRun: true,
+    dryRun: false,
     migrationsTable: 'pgmigrations',
     verbose: true
   });
-  response.status(200).json(migrations);
 }
