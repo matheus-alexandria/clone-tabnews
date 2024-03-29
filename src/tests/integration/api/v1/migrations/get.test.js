595e9bc7ea6b90bde7e0f355244dea312375f9a5
Matchobas <maalexandria@hotmail.com>
2024-03-29 00:20:42 -0300

    test: update get test to work with always empty databse

--- src/tests/integration/api/v1/migrations/get.test.js
+++ src/tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,10 @@
+import database from 'infra/database';
+
 it('should GET /api/v1/migrations and return a 200 status code', async () => {
-  const response = await fetch(`http://localhost:3000/api/v1/migrations`);
+  const response = await fetch(`http://localhost:${process.env.PORT}/api/v1/migrations`);
   const responseBody = await response.json();
 
   expect(response.status).toBe(200);
   expect(Array.isArray(responseBody)).toBeTruthy();
+  expect(responseBody.length).toBeGreaterThan(0);
 });
