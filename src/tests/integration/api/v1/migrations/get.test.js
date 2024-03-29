4262598f02e7308b65745fe3aae6c170eeb99d05
Matchobas <maalexandria@hotmail.com>
2024-03-29 12:14:48 -0300

    test: clean database before test

--- src/tests/integration/api/v1/migrations/get.test.js
+++ src/tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,16 @@
-import database from 'infra/database';
+import { cleanDatabase } from 'src/tests/utils/cleanDatabase'
 
-it('should GET /api/v1/migrations and return a 200 status code', async () => {
-  const response = await fetch(`http://localhost:${process.env.PORT}/api/v1/migrations`);
-  const responseBody = await response.json();
+describe('Dry Run migrations', () => {
+  beforeAll(async () => {
+    await cleanDatabase();
+  });
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody)).toBeTruthy();
-  expect(responseBody.length).toBeGreaterThan(0);
+  it('should GET /api/v1/migrations and return a 200 status code', async () => {
+    const response = await fetch(`http://localhost:${process.env.PORT}/api/v1/migrations`);
+    const responseBody = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(Array.isArray(responseBody)).toBeTruthy();
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
 });
