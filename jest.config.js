83424557cf1d0330f4f2f556e1c5bcd596d9ebdb
Matchobas <maalexandria@hotmail.com>
2024-03-29 12:13:42 -0300

    refactor: loan `.env.development` inside jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
+const dotenv = require('dotenv');
 const nextJest = require('next/jest');
 
-const createJestConfig = nextJest();
+dotenv.config({
+  path: '.env.development'
+});
+
+const createJestConfig = nextJest({
+  dir: '.'
+});
 const jestConfig = createJestConfig({
-  moduleDirectories: ['node_modules', '<rootDir>'],
-  setupFiles: ['<rootDir>/src/tests/dotenv-config.js']
+  moduleDirectories: ['node_modules', '<rootDir>']
 });
 
 module.exports = jestConfig;
